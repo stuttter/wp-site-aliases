245ce8d6aab5274c156d39772d796fef3246295a
John James Jacoby <johnjamesjacoby@me.com>
2016-08-31 09:18:55 -0500

    Clean up metadata functions.

--- wp-site-aliases/includes/functions/metadata.php
+++ wp-site-aliases/includes/functions/metadata.php
@@ -4,9 +4,9 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Add meta data field to a alias.
+ * Add metadata to an alias.
  *
- * @since 0.1.0
+ * @since 1.0.0
  *
  * @param int    $id         Alias ID.
  * @param string $meta_key   Metadata name.
@@ -20,13 +20,13 @@ function add_alias_meta( $id, $meta_key, $meta_value, $unique = false ) {
 }
 
 /**
- * Remove metadata matching criteria from a alias.
+ * Remove from an alias, metadata matching key and/or value.
  *
  * You can match based on the key, or key and value. Removing based on key and
  * value, will keep from removing duplicate metadata with the same key. It also
  * allows removing all metadata matching key, if needed.
  *
- * @since 0.1.0
+ * @since 1.0.0
  *
  * @param int    $id         Alias ID.
  * @param string $meta_key   Metadata name.
@@ -39,9 +39,9 @@ function delete_alias_meta( $id, $meta_key, $meta_value = '' ) {
 }
 
 /**
- * Retrieve relatioship meta field for a alias.
+ * Retrieve from an alias, metadata value by key.
  *
- * @since 0.1.0
+ * @since 1.0.0
  *
  * @param int    $id        Alias ID.
  * @param string $meta_key  Optional. The meta key to retrieve. By default, returns
@@ -55,14 +55,14 @@ function get_alias_meta( $id, $meta_key = '', $single = false ) {
 }
 
 /**
- * Update alias meta field based on alias ID.
+ * Update metadata for an alias ID, and/or key, and/or value.
  *
  * Use the $prev_value parameter to differentiate between meta fields with the
  * same key and alias ID.
  *
  * If the meta field for the alias does not exist, it will be added.
  *
- * @since 0.1.0
+ * @since 1.0.0
  *
  * @param int    $id         Alias ID.
  * @param string $meta_key   Metadata key.
@@ -75,4 +75,3 @@ function get_alias_meta( $id, $meta_key = '', $single = false ) {
 function update_alias_meta( $id, $meta_key, $meta_value, $prev_value = '' ) {
 	return update_metadata( 'blog_aliasmeta', $id, $meta_key, $meta_value, $prev_value );
 }
-
