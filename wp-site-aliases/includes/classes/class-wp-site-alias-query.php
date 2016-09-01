332a6730024799599488be53d4b8d90a0003ca56
John James Jacoby <johnjamesjacoby@me.com>
2016-09-01 18:30:25 -0500

    Update meta & date query clauses.
    Also add `db` object to avoid `$wpdb` global touches.

--- wp-site-aliases/includes/classes/class-wp-site-alias-query.php
+++ wp-site-aliases/includes/classes/class-wp-site-alias-query.php
@@ -104,6 +104,15 @@ class WP_Site_Alias_Query {
 	public $max_num_pages = 0;
 
 	/**
+	 * The database object
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var WPDB
+	 */
+	private $db;
+
+	/**
 	 * Sets up the site alias query, based on the query vars passed.
 	 *
 	 * @since 1.0.0
@@ -149,6 +158,7 @@ class WP_Site_Alias_Query {
 	 * }
 	 */
 	public function __construct( $query = '' ) {
+		$this->db = $GLOBALS['wpdb'];
 		$this->query_var_defaults = array(
 			'fields'            => '',
 			'ID'                => '',
@@ -325,13 +335,9 @@ class WP_Site_Alias_Query {
 	 * @since 1.0.0
 	 * @access protected
 	 *
-	 * @global wpdb $wpdb WordPress database abstraction object.
-	 *
 	 * @return int|array A single count of site alias IDs if a count query. An array of site alias IDs if a full query.
 	 */
 	protected function get_alias_ids() {
-		global $wpdb;
-
 		$order = $this->parse_order( $this->query_vars['order'] );
 
 		// Disable ORDER BY with 'none', an empty array, or boolean false.
@@ -358,7 +364,7 @@ class WP_Site_Alias_Query {
 
 				$parsed = $this->parse_orderby( $_orderby );
 
-				if ( ! $parsed ) {
+				if ( empty( $parsed ) ) {
 					continue;
 				}
 
@@ -372,7 +378,7 @@ class WP_Site_Alias_Query {
 
 			$orderby = implode( ', ', $orderby_array );
 		} else {
-			$orderby = "id {$order}";
+			$orderby = "ba.id {$order}";
 		}
 
 		$number = absint( $this->query_vars['number'] );
@@ -389,66 +395,66 @@ class WP_Site_Alias_Query {
 		if ( $this->query_vars['count'] ) {
 			$fields = 'COUNT(*)';
 		} else {
-			$fields = 'id';
+			$fields = 'ba.id';
 		}
 
 		// Parse site alias IDs for an IN clause.
 		$alias_id = absint( $this->query_vars['ID'] );
 		if ( ! empty( $alias_id ) ) {
-			$this->sql_clauses['where']['ID'] = $wpdb->prepare( 'id = %d', $alias_id );
+			$this->sql_clauses['where']['ID'] = $this->db->prepare( 'ba.id = %d', $alias_id );
 		}
 
 		// Parse site alias IDs for an IN clause.
 		if ( ! empty( $this->query_vars['alias__in'] ) ) {
-			$this->sql_clauses['where']['alias__in'] = "id IN ( " . implode( ',', wp_parse_id_list( $this->query_vars['site__in'] ) ) . ' )';
+			$this->sql_clauses['where']['alias__in'] = "ba.id IN ( " . implode( ',', wp_parse_id_list( $this->query_vars['site__in'] ) ) . ' )';
 		}
 
 		// Parse site alias IDs for a NOT IN clause.
 		if ( ! empty( $this->query_vars['alias__not_in'] ) ) {
-			$this->sql_clauses['where']['alias__not_in'] = "id NOT IN ( " . implode( ',', wp_parse_id_list( $this->query_vars['site__not_in'] ) ) . ' )';
+			$this->sql_clauses['where']['alias__not_in'] = "ba.id NOT IN ( " . implode( ',', wp_parse_id_list( $this->query_vars['site__not_in'] ) ) . ' )';
 		}
 
 		$site_id = absint( $this->query_vars['site_id'] );
 		if ( ! empty( $site_id ) ) {
-			$this->sql_clauses['where']['site_id'] = $wpdb->prepare( 'blog_id = %d', $site_id );
+			$this->sql_clauses['where']['site_id'] = $this->db->prepare( 'ba.blog_id = %d', $site_id );
 		}
 
 		// Parse site IDs for an IN clause.
 		if ( ! empty( $this->query_vars['site__in'] ) ) {
-			$this->sql_clauses['where']['site__in'] = "blog_id IN ( " . implode( ',', wp_parse_id_list( $this->query_vars['site__in'] ) ) . ' )';
+			$this->sql_clauses['where']['site__in'] = "ba.blog_id IN ( " . implode( ',', wp_parse_id_list( $this->query_vars['site__in'] ) ) . ' )';
 		}
 
 		// Parse site IDs for a NOT IN clause.
 		if ( ! empty( $this->query_vars['site__not_in'] ) ) {
-			$this->sql_clauses['where']['site__not_in'] = "blog_id NOT IN ( " . implode( ',', wp_parse_id_list( $this->query_vars['site__not_in'] ) ) . ' )';
+			$this->sql_clauses['where']['site__not_in'] = "ba.blog_id NOT IN ( " . implode( ',', wp_parse_id_list( $this->query_vars['site__not_in'] ) ) . ' )';
 		}
 
 		if ( ! empty( $this->query_vars['domain'] ) ) {
-			$this->sql_clauses['where']['domain'] = $wpdb->prepare( 'domain = %s', $this->query_vars['domain'] );
+			$this->sql_clauses['where']['domain'] = $this->db->prepare( 'ba.domain = %s', $this->query_vars['domain'] );
 		}
 
 		// Parse site alias domain for an IN clause.
 		if ( is_array( $this->query_vars['domain__in'] ) ) {
-			$this->sql_clauses['where']['domain__in'] = "domain IN ( '" . implode( "', '", $wpdb->_escape( $this->query_vars['domain__in'] ) ) . "' )";
+			$this->sql_clauses['where']['domain__in'] = "ba.domain IN ( '" . implode( "', '", $this->db->_escape( $this->query_vars['domain__in'] ) ) . "' )";
 		}
 
 		// Parse site alias domain for a NOT IN clause.
 		if ( is_array( $this->query_vars['domain__not_in'] ) ) {
-			$this->sql_clauses['where']['domain__not_in'] = "domain NOT IN ( '" . implode( "', '", $wpdb->_escape( $this->query_vars['domain__not_in'] ) ) . "' )";
+			$this->sql_clauses['where']['domain__not_in'] = "ba.domain NOT IN ( '" . implode( "', '", $this->db->_escape( $this->query_vars['domain__not_in'] ) ) . "' )";
 		}
 
 		if ( ! empty( $this->query_vars['status'] ) ) {
-			$this->sql_clauses['where']['status'] = $wpdb->prepare( 'path = %s', $this->query_vars['status'] );
+			$this->sql_clauses['where']['status'] = $this->db->prepare( 'ba.path = %s', $this->query_vars['status'] );
 		}
 
 		// Parse site alias status for an IN clause.
 		if ( is_array( $this->query_vars['status__in'] ) ) {
-			$this->sql_clauses['where']['status__in'] = "status IN ( '" . implode( "', '", $wpdb->_escape( $this->query_vars['status__in'] ) ) . "' )";
+			$this->sql_clauses['where']['status__in'] = "ba.status IN ( '" . implode( "', '", $this->db->_escape( $this->query_vars['status__in'] ) ) . "' )";
 		}
 
 		// Parse site alias status for a NOT IN clause.
 		if ( is_array( $this->query_vars['status__not_in'] ) ) {
-			$this->sql_clauses['where']['status__not_in'] = "status NOT IN ( '" . implode( "', '", $wpdb->_escape( $this->query_vars['status__not_in'] ) ) . "' )";
+			$this->sql_clauses['where']['status__not_in'] = "ba.status NOT IN ( '" . implode( "', '", $this->db->_escape( $this->query_vars['status__not_in'] ) ) . "' )";
 		}
 
 		// Falsey search strings are ignored.
@@ -481,14 +487,18 @@ class WP_Site_Alias_Query {
 
 		$date_query = $this->query_vars['date_query'];
 		if ( ! empty( $date_query ) && is_array( $date_query ) ) {
-			$this->date_query = new WP_Date_Query( $date_query, 'registered' );
+			$this->date_query = new WP_Date_Query( $date_query, 'ba.created' );
 			$this->sql_clauses['where']['date_query'] = preg_replace( '/^\s*AND\s*/', '', $this->date_query->get_sql() );
 		}
 
 		$meta_query = $this->query_vars['meta_query'];
 		if ( ! empty( $meta_query ) && is_array( $meta_query ) ) {
-			$this->meta_query = new WP_Meta_Query( $meta_query );
-			$this->sql_clauses['where']['meta_query'] = preg_replace( '/^\s*AND\s*/', '', $this->meta_query->get_sql( 'blog_alias', $wpdb->blog_aliases, 'id', $this ) );
+			$this->meta_query                         = new WP_Meta_Query( $meta_query );
+			$clauses                                  = $this->meta_query->get_sql( 'blog_alias', 'ba', 'id', $this );
+			$join                                     = $clauses['join'];
+			$this->sql_clauses['where']['meta_query'] = preg_replace( '/^\s*AND\s*/', '', $clauses['where'] );
+		} else {
+			$join = '';
 		}
 
 		$where = implode( ' AND ', $this->sql_clauses['where'] );
@@ -530,18 +540,18 @@ class WP_Site_Alias_Query {
 		}
 
 		$this->sql_clauses['select']  = "SELECT {$found_rows} {$fields}";
-		$this->sql_clauses['from']    = "FROM {$wpdb->blog_aliases} {$join}";
+		$this->sql_clauses['from']    = "FROM {$this->db->blog_aliases} ba {$join}";
 		$this->sql_clauses['groupby'] = $groupby;
 		$this->sql_clauses['orderby'] = $orderby;
 		$this->sql_clauses['limits']  = $limits;
 
 		$this->request = "{$this->sql_clauses['select']} {$this->sql_clauses['from']} {$where} {$this->sql_clauses['groupby']} {$this->sql_clauses['orderby']} {$this->sql_clauses['limits']}";
-
+var_dump( $this->request );
 		if ( $this->query_vars['count'] ) {
-			return intval( $wpdb->get_var( $this->request ) );
+			return intval( $this->db->get_var( $this->request ) );
 		}
 
-		$alias_ids = $wpdb->get_col( $this->request );
+		$alias_ids = $this->db->get_col( $this->request );
 
 		return array_map( 'intval', $alias_ids );
 	}
@@ -553,11 +563,9 @@ class WP_Site_Alias_Query {
 	 * @since 1.0.0
 	 * @access private
 	 *
-	 * @global wpdb  $wpdb      WordPress database abstraction object.
 	 * @param  array $alias_ids Optional array of alias IDs
 	 */
 	private function set_found_site_aliases( $alias_ids = array() ) {
-		global $wpdb;
 
 		if ( ! empty( $this->query_vars['number'] ) && ! empty( $this->query_vars['no_found_rows'] ) ) {
 			/**
@@ -570,7 +578,7 @@ class WP_Site_Alias_Query {
 			 */
 			$found_site_aliases_query = apply_filters( 'found_site_aliases_query', 'SELECT FOUND_ROWS()', $this );
 
-			$this->found_site_aliases = (int) $wpdb->get_var( $found_site_aliases_query );
+			$this->found_site_aliases = (int) $this->db->get_var( $found_site_aliases_query );
 		} elseif ( ! empty( $alias_ids ) ) {
 			$this->found_site_aliases = count( $alias_ids );
 		}
@@ -582,24 +590,21 @@ class WP_Site_Alias_Query {
 	 * @since 1.0.0
 	 * @access protected
 	 *
-	 * @global wpdb  $wpdb WordPress database abstraction object.
-	 *
 	 * @param string $string  Search string.
 	 * @param array  $columns Columns to search.
 	 * @return string Search SQL.
 	 */
 	protected function get_search_sql( $string, $columns ) {
-		global $wpdb;
 
 		if ( false !== strpos( $string, '*' ) ) {
-			$like = '%' . implode( '%', array_map( array( $wpdb, 'esc_like' ), explode( '*', $string ) ) ) . '%';
+			$like = '%' . implode( '%', array_map( array( $this->db, 'esc_like' ), explode( '*', $string ) ) ) . '%';
 		} else {
-			$like = '%' . $wpdb->esc_like( $string ) . '%';
+			$like = '%' . $this->db->esc_like( $string ) . '%';
 		}
 
 		$searches = array();
 		foreach ( $columns as $column ) {
-			$searches[] = $wpdb->prepare( "$column LIKE %s", $like );
+			$searches[] = $this->db->prepare( "$column LIKE %s", $like );
 		}
 
 		return '(' . implode( ' OR ', $searches ) . ')';
@@ -611,30 +616,27 @@ class WP_Site_Alias_Query {
 	 * @since 1.0.0
 	 * @access protected
 	 *
-	 * @global wpdb $wpdb WordPress database abstraction object.
-	 *
 	 * @param string $orderby Alias for the field to order by.
 	 * @return string|false Value to used in the ORDER clause. False otherwise.
 	 */
 	protected function parse_orderby( $orderby ) {
-		global $wpdb;
 
 		$parsed = false;
 
 		switch ( $orderby ) {
 			case 'id':
-				$parsed = 'id';
+				$parsed = 'ba.id';
 				break;
 			case 'site_id':
-				$parsed = 'blog_id';
+				$parsed = 'ba.blog_id';
 				break;
 			case 'alias__in':
 				$alias__in = implode( ',', array_map( 'absint', $this->query_vars['alias__in'] ) );
-				$parsed = "FIELD( {$wpdb->blog_aliases}.id, $alias__in )";
+				$parsed = "FIELD( ba.id, $alias__in )";
 				break;
 			case 'site__in':
 				$site__in = implode( ',', array_map( 'absint', $this->query_vars['site__in'] ) );
-				$parsed = "FIELD( {$wpdb->blog_aliases}.blog_id, $site__in )";
+				$parsed = "FIELD( ba.blog_id, $site__in )";
 				break;
 			case 'domain':
 			case 'created':
